b06a5b3fafc147a165867651ed098436f6854007
Michael Dickens <quezlatch@gmail.com>
2019-02-26 07:14:08 +0000

    add more events

--- MancFug.StopLossKata/Tests.fs
+++ MancFug.StopLossKata/Tests.fs
@@ -4,21 +4,28 @@ open System
 open Xunit
 
 type Event =
+    | PriceChanged of int * DateTime
     | PriceHeldAt of int * DateTime
+    | CheckPriceHeldLogged of int * DateTime
 
 type Command =
     | StockIsBought of int * DateTime
     | StockPriceChanges of int * DateTime
+    | CheckIfPriceShouldBeHeld
 
 module Handler =
-    let stockBought (change, date) = [ PriceHeldAt (change, date)]
-    let stockPriceChange (givenHistory : Event list) (change,date) = 
-      []
+    let stockBought (change, date) = [
+      PriceChanged (change, date) 
+      PriceHeldAt (change, date) ]
+    let stockPriceChange (change,date:DateTime) = [
+      PriceChanged (change, date) 
+      CheckPriceHeldLogged(change, date.AddSeconds 15.0)]
 
 let execute (givenHistory : Event list) (command : Command) : Event list =
   match command with
   | StockIsBought (change,date) -> Handler.stockBought (change, date)
-  | StockPriceChanges (change,date) -> Handler.stockPriceChange givenHistory (change, date)
+  | StockPriceChanges (change,date) -> Handler.stockPriceChange (change, date)
+  | CheckIfPriceShouldBeHeld -> []
     
 let Given (events : Event list) = events
 
@@ -43,4 +50,15 @@ let baseLine = DateTime.Now
 let ``Stock is bought`` () =
   Given []
   |> When (StockIsBought (10, baseLine))
-  |> ThenExpect [PriceHeldAt (10, baseLine)]
+  |> ThenExpect [
+    PriceChanged (10, baseLine) 
+    PriceHeldAt (10, baseLine) ]
+
+[<Fact>]
+let ``when price changes our future selves should check if it should be held`` () =
+  Given [PriceHeldAt (10, baseLine)]
+  |> When (StockPriceChanges (11, baseLine.AddSeconds 10.0))
+  |> ThenExpect [
+    PriceChanged (11, baseLine.AddSeconds 10.0)
+    CheckPriceHeldLogged (11, baseLine.AddSeconds 25.0) ]
+
